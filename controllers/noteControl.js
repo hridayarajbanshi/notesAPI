534f26b22793186e121f86620df2cb6eb43bb33a
Sugam Pokhrel <psugam75@gmail.com>
2023-12-09 12:58:21 +0545

    remvoed required field

--- controllers/noteControl.js
+++ controllers/noteControl.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 const noteModal = require('../models/note');
 const createNote = async(req, res)=>{
   const {title, description} = req.body;
+  if(!title){
+    return res.status(400).json({message: "Title is required"});
+  }
+  if(!description){
+    return res.status(400).json({message: "Description is required"});
+  }
   const newNote = new noteModal({
-    title: title,
-    description: description,
-    userId: req.userId
+    title: req.body.title,
+    description: req.body.description,
+    userId: req.userId,
   });
-
   try {
     await newNote.save();
     res.status(200).json(newNote);
