fe3b9b75a23033e53aaa18fb219327a4a3692b70
Sugam Pokhrel <psugam75@gmail.com>
2023-12-09 12:56:31 +0545

    remvoed required field

--- models/note.js
+++ models/note.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const NoteSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    
   },
  description: {
     type: String,
-    required: true,
+   
   },
   userId:{
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    
 
   }
 
